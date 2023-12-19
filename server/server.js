ebac79b63905941762bde7351476dc9b84e74d54
natalie <hardman.natalie85@gmail.com>
2023-12-19 15:41:11 +0000

    correction to server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.get("/", function (req, res) {
     response.json("You are looking at the root route")
 })
 
-app.post("/messages", function (req, res) {
+app.post("/messages", function (request, response) {
     const newMessage = request.body
     console.log(newMessage)
     response.json(newMessage)
